507fffe11940ce0b3741950dddfb7ce66d887ef4
Roberto <xyzistrobertovelaiii@gmail.com>
2024-10-22 12:47:52 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const ctaButton = document.querySelector('.cta button');
+    
+    ctaButton.addEventListener('click', function() {
+        alert('Call 555-123-4567 to claim your $15 off first-time customer special!');
+    });
+});
